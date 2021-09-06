a014525ce598cc323e826e35d7c01f74f7f3e87c
malashonock <malashonock@gmail.com>
2021-09-06 23:28:46 +0300

    feat: replace zeros with arrays of possible digits

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 function traverseMatrix(matrix, callback) {
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[0].length; j++) {
-      callback(matrix[i][j], i, j);
+      callback(matrix[i][j], i, j, matrix);
     }
   }
 }
@@ -54,23 +54,48 @@ function getUnusedDigits(quadrant) {
 const DIGITS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function solveSudoku(matrix) {
-  const sudoku = [...matrix]; // clone array
+  let sudoku = [...matrix]; // clone array
 
+  // Step 1: in each quadrant, replace zeros with arrays of unused digits
+  const quadrants = [];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      quadrants.push(getQuadrant(sudoku, i, j));
+    }
+  }
+
+  const unusedDigits = quadrants.map((q) => getUnusedDigits(q));
+
+  quadrants.map((q, i) => {
+    traverseMatrix(q, (value, row, col, q) => {
+      if (value === 0) {
+        q[row][col] = unusedDigits[i];
+      }
+    });
+  });
+
+  sudoku = combineQuadrants(quadrants);
+
+  // Step 2: for each array cell, remove solid digits that are present in its row/column
+  traverseMatrix(sudoku, (cellContent, row, col, sudoku) => {
+    if (Array.isArray(cellContent)) {
+      // ...
+    }
+  });
   
-
   return sudoku;
 }
 
 const sudokuToSolve = [
-  [5, 3, 4, 6, 7, 8, 9, 0, 0],
-  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  [0, 5, 0, 0, 7, 0, 0, 0, 1],
+  [8, 7, 6, 0, 2, 1, 9, 0, 3],
+  [0, 0, 0, 0, 3, 5, 0, 0, 0],
+  [0, 0, 0, 0, 4, 3, 6, 1, 0],
+  [0, 4, 0, 0, 0, 9, 0, 0, 2],
+  [0, 1, 2, 0, 5, 0, 0, 0, 4],
+  [0, 8, 9, 0, 6, 4, 0, 0, 0],
+  [0, 0, 0, 0, 0, 7, 0, 0, 0],
+  [1, 6, 7, 0, 0, 2, 5, 4, 0]
 ];
 
 const solvedSudoku = [
@@ -85,18 +110,6 @@ const solvedSudoku = [
   [3, 4, 5, 2, 8, 6, 1, 7, 9]
 ];
 
-const q1 = getQuadrant(solvedSudoku, 0, 0);
-const q2 = getQuadrant(solvedSudoku, 0, 1);
-const q3 = getQuadrant(solvedSudoku, 0, 2);
-const q4 = getQuadrant(solvedSudoku, 1, 0);
-const q5 = getQuadrant(solvedSudoku, 1, 1);
-const q6 = getQuadrant(solvedSudoku, 1, 2);
-const q7 = getQuadrant(solvedSudoku, 2, 0);
-const q8 = getQuadrant(solvedSudoku, 2, 1);
-const q9 = getQuadrant(solvedSudoku, 2, 2);
-
-const quadrants = [q1, q2, q3, q4, q5, q6, q7, q8, q9];
-
-console.log(combineQuadrants(quadrants));
+solveSudoku(sudokuToSolve);
 
 module.exports = solveSudoku;
