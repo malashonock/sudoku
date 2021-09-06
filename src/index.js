221547d7424512fb2e5f6ed96806fc9d1b55bc71
malashonock <malashonock@gmail.com>
2021-09-06 22:26:35 +0300

    feat: implement getUnusedDigits function

--- src/index.js
+++ src/index.js
@@ -23,13 +23,39 @@ function getQuadrant(sudoku, rowIndex, colIndex) {
   return quadrant;
 }
 
+function getUnusedDigits(quadrant) {
+  const digits = DIGITS;
+
+  traverseMatrix(quadrant, (value, row, col) => {
+    if (DIGITS.includes(value)) {
+      digits.splice(digits.indexOf(value), 1);
+    }
+  });
+
+  return digits;
+}
+
+const DIGITS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 function solveSudoku(matrix) {
 
 
   return matrix;
 }
 
-const testSudoku = [
+const sudokuToSolve = [
+  [5, 3, 4, 6, 7, 8, 9, 0, 0],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+];
+
+const solvedSudoku = [
   [5, 3, 4, 6, 7, 8, 9, 1, 2],
   [6, 7, 2, 1, 9, 5, 3, 4, 8],
   [1, 9, 8, 3, 4, 2, 5, 6, 7],
@@ -41,6 +67,7 @@ const testSudoku = [
   [3, 4, 5, 2, 8, 6, 1, 7, 9]
 ];
 
-console.log(getQuadrant(testSudoku, 2, 2));
+const quadrant = getQuadrant(sudokuToSolve, 0, 0)
+console.log(getUnusedDigits(quadrant));
 
 module.exports = solveSudoku;
